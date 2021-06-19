30e7b3f812a929db7b1a1659386e4489f48c2a27
EnderC00kiez <ike.welborn@gmal.com>
2021-06-19 19:58:35 -0400

    Added execdyn

---
+++ main.py
@@ -0,0 +1,25 @@
+try:
+    print("Mamba version 1.7 (Canary build)")
+    print("Mamba is loading modules...")
+    import requests
+    import random
+    import time
+
+    print("Mamba finished loading modules, now loading Mamba's libraries into the Python process.")
+
+    def loop(times, loopcode):
+        mambaloopobject = 0
+        for mambaloopobject in range(times):
+            exec(loopcode)
+
+    def get(url, plaintext):
+        if plaintext:
+            mambagetobj = requests.get(url)
+            mambaget = requests.get(url)
+        else:
+            mambaget = requests.get(url)
+    def execdyn(code):
+        eval(code)
+except BaseException as error:
+    print("An error occured in the Mamba runtime. Error: " + str(error) + " (In Python form, not Mamba form.)")
+    raise RuntimeError("Mamba error occured.")
