1b74360b59a530f3a74280dbf97c6f199dfae8ff
EnderC00kiez <ike.welborn@gmal.com>
2021-06-22 08:39:53 -0400

    Added IOSAuth Framework

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ try:
             return mambaget
     def execdyn(code):
         eval(code)
+
+    def iosauth(correctusername, correctpassword, authname):
+        import console
+        while True:
+            mambaauth = console.login_alert(authname)
+            if auth == (correctusername, correctpassword)
+                sleep(0.2)
+                console.hud_alert("Signed in.")
+            else:
+                console.hud_alert("Try Again.", "error")
+                
 except BaseException as error:
     print("An error occured in the Mamba runtime. Error: " + str(error) + " (In Python form, not Mamba form.)")
     raise RuntimeError("Mamba error occured. Please check the program.")
