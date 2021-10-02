0f4014ffa7e207ca2747ade32e4d4ece8813ed61
EnderC00kiez <49785023+EnderC00kiez@users.noreply.github.com>
2021-10-02 16:25:28 -0400

    Update and rename mamba.py to cobra.py

--- cobra.py
+++ cobra.py
@@ -1,25 +1,22 @@
 try:
-    print("Mamba version 1.7 (Canary build)")
-    print("Mamba is loading modules...")
     import requests
     import random
     import time
 
-    print("Mamba finished loading modules, now loading Mamba's libraries into the Python process.")
 
     def loop(times, loopcode):
-        mambaloopobject = 0
-        for mambaloopobject in range(times):
+        cobraloopobject = 0
+        for cobraloopobject in range(times):
             exec(loopcode)
 
     def get(url, plaintext):
         if plaintext:
-            mambagetobj = requests.get(url)
-            mambaget = mambagetobj.text
-            return mambaget
+            cobragetobj = requests.get(url)
+            cobraget = mambagetobj.text
+            return cobraget
         else:
-            mambaget = requests.get(url)
-            return mambaget
+            cobraget = requests.get(url)
+            return cobraget
     def execdyn(code):
         eval(code)
 
@@ -28,8 +25,8 @@ try:
         try:
             import console
             while True:
-                mambaauth = console.login_alert(authname)
-                if auth == (correctusername, correctpassword):
+                cobraauth = console.login_alert(authname)
+                if cobraauth == (correctusername, correctpassword):
                     sleep(0.2)
                     console.hud_alert("Signed in.")
                 else:
@@ -40,5 +37,5 @@ try:
               
                 
 except BaseException as error:
-    print("An error occured in the Mamba runtime. Error: " + str(error) + " (In Python form, not Mamba form.)")
+    print("An error occured in the Mamba runtime. Error: " + str(error) + " (In Python form, not Cobra form.)")
     raise RuntimeError("Mamba error occured. Please check the program.")
